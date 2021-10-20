9fe3b076396d92c75974b9588979a344bb1ed07e
mlh-maishabd23 <abdullah.maisha@yahoo.ca>
2021-10-20 12:39:31 -0400

    Added rollDice() method to class Dice.

--- src/Dice.java
+++ src/Dice.java
@@ -1,5 +1,13 @@
-public class Dice {
-    private int dice;
+import java.util.Random;
 
+public class Dice {
+
+    private int diceNumber;
+
+    public int rollDice(){
+        Random rn = new Random();
+        this.diceNumber = rn.nextInt(11)+2;
+        return this.diceNumber;
+    }
 
 }
