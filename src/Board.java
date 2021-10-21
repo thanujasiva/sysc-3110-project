fea6aea9b383cea42fe981e86162b7b5bd859e62
samwatin <sabahsamwatin@cmail.carleton.ca>
2021-10-21 22:13:35 +0600

    editted board and player

--- src/Board.java
+++ src/Board.java
@@ -7,5 +7,31 @@ public class Board {
     private HashMap<Integer, Box> boxes;
     private ArrayList<Player> players;
     private Player currentPlayer;
+    private Player playerNumber;
 
+    public Board (){
+        this.dice= new Dice();
+        this.boxes = new HashMap<>();
+        this.players = new ArrayList<>(2);
+        this.addPlayer(new Player());
+        this.addPlayer(new Player());
+        this.currentPlayer = players.get(0);
+    }
+
+    private void addPlayer (Player newPlayer) {
+
+        int length = players.size();
+        newPlayer.setId(length+1);
+        players.add(newPlayer);
+
+    }
+    private void play(){
+
+
+    }
+    public static void main(String[] args) {
+        Board board = new Board();
+
+
+    }
 }
