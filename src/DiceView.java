6dfc8ac935ca6fc61f76bd7e218795ad46890bcd
samwatin <sabahsamwatin@cmail.carleton.ca>
2021-11-06 17:56:32 +0600

    Added PlayersView and DiceView classes. Made some minor edits on Board.java and Dice.java. Changed 1 rolling dice to 2.

---
+++ src/DiceView.java
@@ -0,0 +1,70 @@
+import javax.swing.*;
+import java.util.Scanner;
+
+public class DiceView {
+    private JPanel dicePanel;
+    private JLabel diceLabel;
+    private Dice dice;
+
+    /**
+     * @author Sabah
+     * DiceView Constructor
+     * @param dice
+     */
+
+    public DiceView(Dice dice) {
+        this.dice = dice;
+        dicePanel = new JPanel();
+        dicePanel.setLayout(new BoxLayout(dicePanel, BoxLayout.Y_AXIS));
+        diceLabel = new JLabel();
+        dicePanel.add(diceLabel);
+        diceLabel.setText(String.valueOf(dice.getDiceNumber()));
+
+    }
+
+    /**
+     * @author Sabah
+     * Updates the dice label when the controller calls
+     */
+    // Controller calls (refer to MVC)
+    public void updateDiceLabel(){
+        diceLabel.setText(String.valueOf(dice.getDiceNumber()));
+    }
+
+    /**
+     * @author Sabah
+     * @return  dicePanel
+     * getter for dicePanel
+     */
+    public JPanel getDicePanel() {
+        return dicePanel;
+    }
+
+    // test method
+    // needs to be removed later
+    public static void main(String[] args) {
+
+        Dice dice = new Dice();
+
+        DiceView diceView = new DiceView(dice);
+        JFrame testFrame = new JFrame("Test frame for Dice View: ");
+        testFrame.add(diceView.getDicePanel());
+
+        testFrame.setSize(300,400); // FIXME - have proper size in the frame
+        testFrame.setVisible(true);
+        testFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        Scanner sc = new Scanner(System.in);
+        String command="";
+        
+        for(int x = 0; x<10; x++){
+            System.out.println("Type anything to roll! ");
+            command = sc.nextLine();
+            int roll = dice.rollDice();
+            diceView.updateDiceLabel();
+            System.out.println(roll);
+        }
+
+    }
+
+}
---
+++ src/PlayersView.java
@@ -0,0 +1,61 @@
+import javax.swing.*;
+
+public class PlayersView {
+
+    private JPanel playersPanel;
+
+    /**
+     * @author Sabah
+     * @param board
+     * Shoes the list of players on the top label
+     * and the current player on the bottom label
+     *
+     */
+
+    public PlayersView(Board board) {
+        playersPanel = new JPanel();
+        playersPanel.setLayout(new BoxLayout(playersPanel, BoxLayout.Y_AXIS));
+
+        JLabel playerNamesLabel = new JLabel("List of Players: ");
+        JLabel currentPlayerLabel = new JLabel("Current Player: " + board.getCurrentPlayerNumber());
+
+        playersPanel.add(playerNamesLabel);
+
+        // adds to the actual list by looping
+        DefaultListModel playersListModel  = new DefaultListModel();
+        for (Player player : board.getPlayers()){
+            playersListModel.addElement("Player: "+ player.getId());
+        }
+
+        // shows the list of players
+        JList playerList = new JList<>(playersListModel);
+        playerList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        playersPanel.add(new JScrollPane(playerList));
+        playersPanel.add(currentPlayerLabel);
+    }
+    /**
+     * @author Sabah
+     * @return  playersPanel
+     * getter for playersPanel
+     */
+    public JPanel getPlayersPanel() {
+        return playersPanel;
+    }
+
+    // test method
+    // can be removed later
+    public static void main(String[] args) {
+
+        Board board = new Board();
+        
+        PlayersView playersView = new PlayersView(board);
+        JFrame testFrame = new JFrame("Test frame for Players View: ");
+        testFrame.add(playersView.getPlayersPanel());
+
+        testFrame.setSize(300,400); // FIXME - have proper size in the frame
+        testFrame.setVisible(true);
+        testFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+    }
+
+}
