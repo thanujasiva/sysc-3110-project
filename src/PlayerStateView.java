69a95fc6fc58638c4e7614f7b9fe0d0a9b9a1238
Thanuja Siva <59773247+thanujasiva@users.noreply.github.com>
2021-11-06 16:45:38 -0400

    Change PlayerStateView to extend JPanel. (#45)

--- src/PlayerStateView.java
+++ src/PlayerStateView.java
@@ -1,9 +1,6 @@
 import javax.swing.*;
 import javax.swing.border.Border;
 import javax.swing.border.EmptyBorder;
-import java.awt.*;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 import java.util.Scanner;
 
 /**
@@ -11,27 +8,28 @@ import java.util.Scanner;
  * Creates a frame for an individual player's state
  * @author Thanuja
  */
-public class PlayerStateView {
-
-    private final JFrame playerStateFrame;
+public class PlayerStateView extends JPanel{
 
     /**
      * @author Thanuja
      * @param player to view the current state
      */
     public PlayerStateView(Player player){
-        JPanel playerStatePanel = new JPanel();
-        playerStatePanel.setLayout(new BoxLayout (playerStatePanel, BoxLayout.Y_AXIS));
+        super();
+
+        this.setLayout(new BoxLayout (this, BoxLayout.Y_AXIS));
 
         Border fieldBorder = new EmptyBorder(6,3,3,3);
 
+        JLabel playerNameLabel = new JLabel("Player " + player.getId());
         JLabel playerMoneyLabel = new JLabel("Money: $" + player.getMoney());
-        playerMoneyLabel.setBorder(fieldBorder);
         JLabel propertiesListHeader = new JLabel("Current properties you own:");
+        playerNameLabel.setBorder(fieldBorder);
+        playerMoneyLabel.setBorder(fieldBorder);
         propertiesListHeader.setBorder(fieldBorder);
-        playerStatePanel.add(playerMoneyLabel);
-        playerStatePanel.add(propertiesListHeader);
-
+        this.add(playerNameLabel);
+        this.add(playerMoneyLabel);
+        this.add(propertiesListHeader);
 
         // create list with the property names of the properties the player owns
         DefaultListModel<String> propertiesModel = new DefaultListModel<>();
@@ -42,7 +40,7 @@ public class PlayerStateView {
         // add property name list to the JPanel
         JList<String> propertiesList = new JList<>(propertiesModel);
         propertiesList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-        playerStatePanel.add(new JScrollPane(propertiesList));
+        this.add(new JScrollPane(propertiesList));
 
         // display cardView for the selected property in the JList
         propertiesList.addListSelectionListener(e -> {
@@ -53,31 +51,14 @@ public class PlayerStateView {
             }
         });
 
-        playerStateFrame = new JFrame("Player " + player.getId());
-        playerStateFrame.setSize(250, 350); // gives a good size to the frame
-        playerStateFrame.add(playerStatePanel);
-        playerStateFrame.setVisible(true);
-
-        playerStateFrame.addWindowListener(new WindowAdapter() {  //defining a class inside another class
-            public void windowClosing(WindowEvent e) {
-                playerStateFrame.dispose();
-            }
-        });
-
-    }
-
-    /**
-     * @author Thanuja
-     * @return player state frame
-     */
-    public JFrame getPlayerStateFrame() {
-        return playerStateFrame;
     }
 
     // test method to view how PlayerStateView would look for a player
     // remove this afterwards
     public static void main(String[] args) {
-        PlayerStateView playerStateView;
+        JFrame playerStateFrame = new JFrame("Test Frame");
+        // only have EXIT_ON_CLOSE in this test method, should not close the actual game
+        playerStateFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
         Property Baltic = new Property("Baltic Avenue", 60, ColourGroups.BROWN);
         Property Oriental = new Property("Oriental Avenue", 100, ColourGroups.GREY);
@@ -91,17 +72,17 @@ public class PlayerStateView {
         player1.purchaseProperty(Illinois);
         player1.purchaseProperty(Atlantic);
 
-        playerStateView = new PlayerStateView(player1);
-        // only have EXIT_ON_CLOSE in this test method, should not close the actual game
-        playerStateView.playerStateFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        PlayerStateView playerStateView1 = new PlayerStateView(player1);
+
+        playerStateFrame.setSize(250, 350); // gives a good size to the frame
+        playerStateFrame.add(playerStateView1);
+        playerStateFrame.setVisible(true);
 
         // testing a change in who the current player is
         Scanner sc = new Scanner(System.in);
         System.out.println("Type anything to change the player");
         sc.nextLine();
 
-        // can call setVisible(false) when the current player's turn is over
-        playerStateView.getPlayerStateFrame().setVisible(false);
 
         // testing with putting a second player
         Player player2 = new Player();
@@ -109,9 +90,14 @@ public class PlayerStateView {
         player2.purchaseProperty(Oriental);
 
         // when it's the next players turn, then call
-        playerStateView = new PlayerStateView(player2); // on the new current player
+        PlayerStateView playerStateView2 = new PlayerStateView(player2); // on the new current player
+        playerStateFrame.remove(playerStateView1);
+        playerStateFrame.add(playerStateView2);
+        playerStateFrame.setVisible(true);
 
-        // only have EXIT_ON_CLOSE in this test method, should not close the actual game
-        playerStateView.playerStateFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        playerStateFrame.invalidate();
+        playerStateFrame.validate();
+        playerStateFrame.repaint();
+
     }
 }
