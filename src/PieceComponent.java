4d78c990afcc21cef0e78a81a3a6a656b1beb25e
shrimeichock <59775096+shrimeichock@users.noreply.github.com>
2021-11-08 09:18:40 -0500

    Move piece (#65)
    * Make piece a label and move from one panel to another with the click of a button

--- src/PieceComponent.java
+++ src/PieceComponent.java
@@ -1,12 +1,21 @@
 import javax.swing.*;
+import javax.swing.plaf.DimensionUIResource;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 // create a panel that you can draw on.
 class PieceComponent extends JPanel {
+    public PieceComponent(){
+        this.setPreferredSize(new Dimension(50,50));
+        this.setOpaque(true);
+        this.setBackground(Color.RED);
+    }
+    /*
     public void paint(Graphics g) {
         g.setColor(Color.red);
         g.fillRect(10,10,50,50);
-    }
+    }*/
 
     public static void main(String[] arguments) {
 
@@ -15,20 +24,57 @@ class PieceComponent extends JPanel {
         JFrame frame = new JFrame("JFrame Color Example");
         frame.setSize(300,200);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        //frame.getContentPane().setLayout(new BoxLayout(frame.getContentPane(),BoxLayout.X_AXIS));
-        JLayeredPane layeredPane = new JLayeredPane();
-        layeredPane.setPreferredSize(new Dimension(300, 300));
-
+        frame.getContentPane().setLayout(new BoxLayout(frame.getContentPane(),BoxLayout.X_AXIS));
+        //JLayeredPane layeredPane = new JLayeredPane();
+        //layeredPane.setPreferredSize(new Dimension(300, 300));
 
         JPanel box1 = new JPanel();
         box1.setOpaque(true);
-        box1.setBackground(Color.YELLOW);
+        box1.setBackground(Color.YELLOW);//sdfdsfs
+
 
         JPanel box2 = new JPanel();
         box2.setOpaque(true);
         box2.setBackground(Color.DARK_GRAY);
 
-        layeredPane.add(box1,JLayeredPane.DEFAULT_LAYER);
+        /*JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        //panel.add(box1);
+        //panel.add(piece);
+
+        JPanel panel2 = new JPanel();
+        panel2.setLayout(new BoxLayout(panel2, BoxLayout.Y_AXIS));
+        //panel2.add(box2);*/
+
+        JLabel label = new JLabel();
+        label.setText("afdsfsdfsd");
+        label.setOpaque(true);
+        label.setBackground(Color.RED);
+        box1.add(label);
+        //box1.remove(label);
+
+
+
+        JButton button = new JButton();
+        /*button.addActionListener(e -> {
+            //panel.remove(piece);
+            //panel2.add(piece);
+            box1.remove(label);
+            //box1.setBackground(Color.RED);
+            //box2.add(label);
+            //piece.repaint();
+        });*/
+
+        button.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                box1.remove(label);
+                box2.add(label);
+                frame.repaint();
+                //label.setText("Welcome to Javatpoint.");
+            }
+        });
+
+        //layeredPane.add(box1,JLayeredPane.DEFAULT_LAYER);
         //layeredPane.add(piece, 3);
         //layeredPane.add(box2,5);
 
@@ -37,12 +83,15 @@ class PieceComponent extends JPanel {
         //pane.add(box2);
 
         //pane.moveToFront(piece);
-        layeredPane.setVisible(true);
+        //layeredPane.setVisible(true);
 
         //frame.add(layeredPane);
-        //frame.add(box2);
+        frame.add(box1);
+        frame.add(box2);
+        frame.add(button);
+        //frame.add(piece);
 
-        //frame.setVisible(true);
+        frame.setVisible(true);
 
     }
 }
