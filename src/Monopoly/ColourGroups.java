ad41996ac53e1e18fa27777d753305da759cf8c6
Shrimei Chock <meichock1@gmail.com>
2021-11-08 18:13:31 -0500

    Add comments to ColourGroups

--- src/Monopoly/ColourGroups.java
+++ src/Monopoly/ColourGroups.java
@@ -2,9 +2,8 @@ package Monopoly;
 
 import java.awt.*;
 
-public enum ColourGroups {
+public enum ColourGroups{
 
-    //FIX BROWN
     BROWN (new Color(153, 102, 0), 2), GREY(new Color(204, 229, 255), 3), PINK(new Color(247,121,247), 3),
     ORANGE(new Color(255, 153, 51), 3), RED(Color.RED, 3), YELLOW(Color.YELLOW, 3),
     GREEN(new Color(0,153,76), 3), BLUE(new Color(0,102,204), 2);
@@ -24,8 +23,7 @@ public enum ColourGroups {
 
     /**
      * @author Shrimei
-     * @return              the colour group
-     * Returns a String representation of the colour
+     * @return the colour group that the property belongs to
      */
     public Color getColour() {
         return colour;
@@ -34,8 +32,7 @@ public enum ColourGroups {
 
     /**
      * @author Shrimei
-     * @return              the maximum cards of the set
-     * Returns the number of properties of that colour required to get a complete colour set
+     * @return  the number of properties of that colour required to get a complete colour set
      */
     public int getMax() {
         return max;
