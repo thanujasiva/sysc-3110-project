bce89154f6cf80b8924ce3f66dfd6a9351aff40d
Maisha Abdullah <59773012+maishabd23@users.noreply.github.com>
2021-11-20 10:35:54 -0500

    Added House Class and hasAllColourGroups method in player. (#119)

---
+++ src/House.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+
+public class House {
+
+    private int price;
+    private int rent1;
+    private Color color;
+
+    public House(int price, int rent1, Color color){
+        this.price = price;
+        this.rent1 = rent1;
+        this.color = color;
+    }
+}
