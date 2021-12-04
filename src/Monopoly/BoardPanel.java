30e85a54d626fb078663e9a579230751c1dca661
Thanuja Siva <59773247+thanujasiva@users.noreply.github.com>
2021-12-04 16:24:52 -0500

    Remove arr attribute in BoardPanel. (#168)

--- src/Monopoly/BoardPanel.java
+++ src/Monopoly/BoardPanel.java
@@ -12,7 +12,6 @@ import java.util.LinkedHashMap;
 
 public class BoardPanel {
     private JPanel mainPanel;
-    private Component[] arr; //FIXME
     private LinkedHashMap<JPanel, Square> allSquares;
 
     /**
@@ -92,7 +91,6 @@ public class BoardPanel {
         for (int i = 0; i < 9; i++){
             JPanel box =  (JPanel) allSquares.keySet().toArray()[8-i];
             bottomBorder.add(box);
-
         }
 
         //left boxes
@@ -106,12 +104,10 @@ public class BoardPanel {
             JPanel box =  (JPanel) allSquares.keySet().toArray()[17+i];
             topBorder.add(box);
         }
-        arr = topBorder.getComponents(); //FIXME
         //right boxes
         for (int i = 0; i < 8; i++){
             JPanel box =  (JPanel) allSquares.keySet().toArray()[26+i];
             rightBorder.add(box);
-            arr = rightBorder.getComponents(); //FIXME
         }
 
         mainPanel.setLayout(new BorderLayout());
